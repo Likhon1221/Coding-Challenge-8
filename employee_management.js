3c08e8dd870e413d3759a92fac2441ee95f0e471
Likhon1221 <siddiqur.rahman1221@gmail.com>
2024-10-06 18:36:36 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -6,8 +6,39 @@ class Employee {
         this.salary = salary;
         this.position = position;
         this.department = department;
-    }
+    };
     getDetails() {
         return `${this.name} Position ${this.position} Salary $${this.salary}`;
-    }
-}
+    };
+};
+
+// Task 2. Create a Department Class
+
+class Department {
+    constructor(name) {
+        this.name = name;
+        this.employees = [];
+    };
+
+    addEmployee(employee) {
+        this.employees.push(employee);
+    };
+
+    getDepartmentSalary() {
+        let totalSalary = 0;
+        for (let i = 0; i < this.employees.length; i++) {
+            totalSalary += this.employees[i].salary;
+        }
+        return totalSalary;
+    };
+
+ // Task 4: Calculate total salary including bonuses using a for loop
+
+    calculateTotalSalaryWithBonus() {
+        let total = 0;
+        for (let i = 0; i < this.employees.length; i++) {
+            total += this.employees[i].getTotalSalary();
+        }
+        return total;
+    };
+};
